08938fd3c02358b5b66d9425f5b54251c244c46f
zpzhangwenhan <858966776@qq.com>
2017-07-24 18:02:10 +0800

    commit

--- Test.py
+++ Test.py
@@ -1,5 +1,8 @@
-a =   100
-if a >= 0:
-    print(a)
-else:
-    print(-a)
+s1 = 72
+s2 = 85
+r = 100*(s2-s1)/s1
+print('小明成绩提升了%.1f%%' % r);
+
+
+classmates = ['Michael', 'Bob', 'Tracy']
+classmates.insert(0,"zz");
